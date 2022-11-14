fe83440dade53bae2c0062c0261bed210c3c0002
amazinggrace88 <kyungjin2348@gmail.com>
2022-11-14 21:02:28 +0900

    feat(crawing): 소상공인시장진흥공단_상가(상권)정보_API - 서울특별시만 제한함

--- crawing.py
+++ crawing.py
@@ -2,18 +2,12 @@ import requests
 import json
 import pandas as pd
 import openpyxl
+import time
 
 """
-소상공인시장진흥공단_상가(상권)정보_API
+example : 소상공인시장진흥공단_상가(상권)정보_API
 """
 # http://apis.data.go.kr/B553077/api/open/sdsc2/오퍼레이션명/필수항목1.../필수항목n?ServiceKey=서비스키&옵션항목1=옵션항목1값......&옵션항목n=옵션항목값n
-
-
-def get_response(key, serviceKey, operation, type):
-    url = f'http://apis.data.go.kr/B553077/{operation}?key={key}&ServiceKey={serviceKey}&type={type}'
-    response = requests.get(url).text
-    return response
-
 
 def get_key():
     # key 가져오기
@@ -22,20 +16,35 @@ def get_key():
     return key_list
 
 
-if __name__ == '__main__':
+def get_response(key, serviceKey, operation, type):
+    url = f'http://apis.data.go.kr/B553077/{operation}?key={key}&ServiceKey={serviceKey}&type={type}'
+    try:
+        response = requests.get(url).json()
+    except:
+        time.sleep(5)
+        response = requests.get(url).json()
+    return response
 
+
+if __name__ == '__main__':
     serviceKey = 'AR4UatAaPl9WoEinY9o0FlCGJz%2FX8mhDxpu4ePf8%2BH5DlpXXjzryeS%2FJQ6r2%2FROB6ik2iIhO4fI9fh1FBYWpUA%3D%3D'
     operation = 'api/open/sdsc2/storeZoneOne'  # 소상공인 상가업소 정보서비스 중 지정 상권조회 오퍼레이션 명세
     type = 'json'
     key_list = get_key()
     for idx, key in enumerate(key_list):
+        print(f'=={idx}, {key} start ==')
         response = get_response(key=key, serviceKey=serviceKey, operation=operation, type=type)
-        response_json = json.loads(response)
-        columns = response_json.get('header')['columns']
-        row = pd.json_normalize(response_json.get('body')['items'])
+        columns = response.get('header')['columns']
+        row = pd.json_normalize(response.get('body')['items'])
         if idx==0:
             df = pd.DataFrame(row)
         else:
-            df = pd.concat([df, row], axis=1)
+            df = pd.concat([df, row], axis=0)
+        print(df)
+        print(f'=={idx}, {key} done ==')
+        time.sleep(10)
+        if key==9249: # 서울특별시만 크롤링
+            break
     df = df.rename(columns=columns)
     print(df)
+    df.to_csv('/dataset/sdsc2_storeZoneOne.csv')
